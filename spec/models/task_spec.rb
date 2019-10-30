b35d1acb5fb238d517d6226b67dedb61b3fde855
hiro <hironori.matsuse@gmail.com>
2019-10-30 20:53:00 +0900

    Add:最初のユーザーをseed実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -43,21 +43,26 @@ RSpec.describe Task, type: :model do
       expect(task).to be_valid
   end
 
-  
+  #ここ
 #振る舞いごとに処理が実行される
+  #describeは対象(〜について) を分類
+  #contextはその状態(〜な場合) を分類
+#1:宣言する
+  context "modelに記載したscopeによる絞りこみを確かめる" do
+#検索フォームで作る.(factorybotで作ったtask_nameを入れる)
+#factorybotの（：task）の名前を変数@taskにいれる（代入）。
   before do
     @task = FactoryBot.create(:task)
   end
-
-  #describeは対象(〜について) を分類
-  #contextはその状態(〜な場合) を分類
-
-  context "modelに記載したscopeによる絞りこみを確かめる" do
     # 検索文字列に一致するメモを返すこと
     it "modelに記載したscopeによる絞りこみを確かめる" do
       #Taskモデルのtask_nameを検索したとき@taskが見つかる
+      #期待する(Taskモデルに記載したscorp(task_name_search)をつかって、test_task_01)が@taskの中に入っている。
       expect(Task.task_name_search("test_task_01")).to include(@task)
     end
+    it "www" do
+      #Taskモデルのtask_nameを検索したとき@taskが見つかる
+      expect(Task.task_status("not_started")).to include(@task)
+    end
   end
-  
 end
