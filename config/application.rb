902dd75303333f8b4e48b2d4953879aa22e821e6
hiro <hironori.matsuse@gmail.com>
2020-02-05 00:39:14 +0900

    Fix:エラーの修正

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module TaskManager
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.assets.initialize_on_precompile = false
     
 
     # Settings in config/environments/* take precedence over those specified here.
